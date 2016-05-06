e5365f97d65f1eef94d31a37c53939cc82f74c09
Henrik Molnes <molnes@gmail.com>
2016-05-06 09:10:20 +0200

    initial commit, setup vs project, installed mahapps nuget

---
+++ ImageDownloader/ImageDownloader/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace ImageDownloader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
