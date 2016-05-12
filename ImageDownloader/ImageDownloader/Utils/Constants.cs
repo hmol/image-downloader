821ee41833312a6cab4079e087af063c1ba38e07
Henrik Molnes <molnes@gmail.com>
2016-05-12 12:56:10 +0200

    added reged for transforming filenames with unvalid chars

--- ImageDownloader/ImageDownloader/Utils/Constants.cs
+++ ImageDownloader/ImageDownloader/Utils/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public const string FileNameRegex = "";
+        public const string FileNameRegex = "[^a-zA-Z0-9 - .]";
         public const string StatusOk = "Ok";
         public const string ImgSrcXpath = "//img[@src]";
         public const string ImgSrcAttribute = "src";
